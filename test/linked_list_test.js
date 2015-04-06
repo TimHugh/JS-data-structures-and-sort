1e837da09b86fe80a6fd29c41d66b02d88fb33bc
Tim Heuett <tim.heuett@gmail.com>
2015-04-06 13:58:20 -0700

    add tests for node removal

--- test/linked_list_test.js
+++ test/linked_list_test.js
@@ -2,26 +2,29 @@ var expect = require('chai').expect;
 var LinkedList = require('../lib/linked_list.js');
 
 describe('Linked list', function() {
-  it('should store new values at the head', function(){
-    var list = new LinkedList();
+  var list = new LinkedList();
 
+  beforeEach(function() {
+    list = new LinkedList();
     list.add(2);
-    expect(list.first()).to.equal(2);
+  });
 
+  it('should store new values at the head', function() {
     list.add(5);
     expect(list.first()).to.equal(5);
   });
 
-  it('should return the correct node when a value is searched', function(){
-    var list = new LinkedList();
-
-    list.add(2);
+  it('should return the correct node when a value is searched', function() {
     expect(list.search(2).value).to.equal(2);
   });
-  it('returns nil when searching for a non-existent value', function(){
-    var list = new LinkedList();
-
-    list.add(2);
+  it('returns null when searching for a non-existent value', function() {
     expect(list.search(3)).to.equal(null);
+  });
+
+  it('returns a value when it removes it from the list', function() {
+    expect(list.remove(2)).to.equal(2);
+  });
+  it('returns null when removing a value that is not present', function() {
+    expect(list.remove(3)).to.equal(null)
   });
 });
