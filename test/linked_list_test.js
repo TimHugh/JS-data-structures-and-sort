53838cc30f5be2ddfec48bc8a5a0e70655bc040b
Tim Heuett <tim.heuett@gmail.com>
2015-04-06 14:04:56 -0700

    add to_s test

--- test/linked_list_test.js
+++ test/linked_list_test.js
@@ -7,6 +7,8 @@ describe('Linked list', function() {
   beforeEach(function() {
     list = new LinkedList();
     list.add(2);
+    list.add(4);
+    list.add(7);
   });
 
   it('should store new values at the head', function() {
@@ -26,5 +28,9 @@ describe('Linked list', function() {
   });
   it('returns null when removing a value that is not present', function() {
     expect(list.remove(3)).to.equal(null)
+  });
+
+  it('creates a comma-separated string when the to_s function is called', function() {
+    expect(list.to_s).to.equal("2, 4, 7");
   });
 });
