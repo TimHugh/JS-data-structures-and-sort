48b64202e7c9f2d2507952668799a624fdc996df
Tim Heuett <tim.heuett@gmail.com>
2015-04-06 13:38:34 -0700

    more thorough add test

--- test/linked_list_test.js
+++ test/linked_list_test.js
@@ -4,8 +4,11 @@ var LinkedList = require('../lib/linked_list.js');
 describe('Linked list', function() {
   it('should store new values at the head', function(){
     var list = new LinkedList();
-    list.add(2);
 
+    list.add(2);
     expect(list.first()).to.equal(2);
+
+    list.add(5);
+    expect(list.first()).to.equal(5);
   });
 });
