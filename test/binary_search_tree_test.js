ab138ad08643a8b81dc743ee36ca0d6cbacaec90
Tim Heuett <tim.heuett@gmail.com>
2015-04-16 22:07:37 -0700

    add depth test

--- test/binary_search_tree_test.js
+++ test/binary_search_tree_test.js
@@ -4,7 +4,7 @@ var BinarySearchTree = require('../lib/binary_search_tree.js')
 describe('Binary Search Tree', function(){
   var tree = null;
 
-  before(function(){
+  beforeEach(function(){
     tree = new BinarySearchTree();
   });
 
@@ -15,5 +15,17 @@ describe('Binary Search Tree', function(){
   it('increments size when an item is added', function(){
     tree.insert(1);
     expect(tree.size).to.equal(1);
+  });
+
+  it('keeps track of the current depth of the tree', function(){
+    expect(tree.depth).to.equal(0);
+    tree.insert(2);
+    expect(tree.depth).to.equal(1);
+    tree.insert(1);
+    expect(tree.depth).to.equal(2);
+    tree.insert(3);
+    expect(tree.depth).to.equal(2);
+    tree.insert(4);
+    expect(tree.depth).to.equal(3);
   });
 });
